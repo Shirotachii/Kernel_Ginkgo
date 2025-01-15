f51b481b4de842b0201970c27f8f773b40006c76
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-15 12:16:52 -0300

    cpufreq: apply init protection for governor
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,6 +33,7 @@
 #include <linux/sched/topology.h>
 #include <linux/sched/sysctl.h>
 #include <linux/battery_saver.h>
+#include <linux/binfmts.h>
 
 #include <trace/events/power.h>
 
@@ -791,6 +792,9 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	int ret;
 	char	str_governor[16];
 	struct cpufreq_policy new_policy;
+
+	if (task_is_booster(current))
+		return -EINVAL;
 
 	memcpy(&new_policy, policy, sizeof(*policy));
 
