5d0935bec7353ae288befe8da7cc8991ab4bd32e
NekoTuru10 <kirmansyah9696@gmail.com>
2025-06-18 06:57:48 +0000

    syscall: Fake uname in uprobestatsbpfload

--- kernel/sys.c
+++ kernel/sys.c
@@ -1196,6 +1196,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "uprobestatsbpfload", 18) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "4.19.325");
 		pr_debug("fake uname: %s/%d release=%s\n",
