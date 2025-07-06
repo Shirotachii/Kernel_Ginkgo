ec2e62faf19c23e230088ddafdf56f545c6bace1
NekoTuru10 <kirmansyah9696@gmail.com>
2025-07-06 15:50:07 +0700

    Revert "drivers/staging/almk: disable tracing"
    This reverts commit 2e77e3443bb093dd480c9d318dd540d01abd18c2.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -221,7 +221,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 		other_free = global_zone_page_state(NR_FREE_PAGES);
 
 		atomic_set(&shift_adj, 1);
-		//trace_almk_vmpressure(pressure, other_free, other_file);
+		trace_almk_vmpressure(pressure, other_free, other_file);
 	} else if (pressure >= 90) {
 		if (lowmem_adj_size < array_size)
 			array_size = lowmem_adj_size;
@@ -237,7 +237,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 		if (other_free < lowmem_minfree[array_size - 1] &&
 		    other_file < vmpressure_file_min) {
 			atomic_set(&shift_adj, 1);
-			//trace_almk_vmpressure(pressure, other_free, other_file);
+			trace_almk_vmpressure(pressure, other_free, other_file);
 		}
 	} else if (atomic_read(&shift_adj)) {
 		other_file = global_node_page_state(NR_FILE_PAGES) -
@@ -251,7 +251,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 		 * Since vmpressure has improved, reset shift_adj to avoid
 		 * false adaptive LMK trigger.
 		 */
-		//trace_almk_vmpressure(pressure, other_free, other_file);
+		trace_almk_vmpressure(pressure, other_free, other_file);
 		atomic_set(&shift_adj, 0);
 	}
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_TNG
@@ -621,7 +621,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		     other_file, min_score_adj);
 
 	if (min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
-//		trace_almk_shrink(0, ret, other_free, other_file, 0);
+		trace_almk_shrink(0, ret, other_free, other_file, 0);
 		lowmem_print(5, "%s %lu, %x, return 0\n",
 			     __func__, sc->nr_to_scan, sc->gfp_mask);
 #ifdef LMK_TNG_ENABLE_TRACE
@@ -748,7 +748,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			}
 		}
 		task_unlock(selected);
-		//trace_lowmemory_kill(selected, cache_size, cache_limit, free);
+		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
 		lowmem_print(1, "Killing '%s' (%d) (tgid %d), adj %hd,\n"
 			"to free %ldkB on behalf of '%s' (%d) because\n"
 			"cache %ldkB is below limit %ldkB for oom score %hd\n"
@@ -790,12 +790,12 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		rcu_read_unlock();
 		/* give the system time to free up the memory */
 		msleep_interruptible(20);
-		//trace_almk_shrink(selected_tasksize, ret,
-		//		  other_free, other_file,
-		//		  selected_oom_score_adj);
+		trace_almk_shrink(selected_tasksize, ret,
+				  other_free, other_file,
+				  selected_oom_score_adj);
 		lmk_inc_stats(LMK_KILL);
 	} else {
-		//trace_almk_shrink(1, ret, other_free, other_file, 0);
+		trace_almk_shrink(1, ret, other_free, other_file, 0);
 		lmk_inc_stats(LMK_WASTE);
 		rcu_read_unlock();
 		if (other_free < lowmem_minfree[0] &&
