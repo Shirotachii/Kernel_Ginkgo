2761fd0bd8f2258c29ffe4a9921faf2ec7335997
NekoTuru10 <kirmansyah9696@gmail.com>
2025-08-16 14:28:29 +0000

    Reapply "kernel: bpf: Pretend to support ANY prog_type/attach_type"
    This reverts commit 52e71bca804b521b0c19f6a046dfb4b1eb4a867e.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1211,6 +1211,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
