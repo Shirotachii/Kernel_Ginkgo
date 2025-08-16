7acdd4c860e700796209a6fc29b8a86a59829488
NekoTuru10 <kirmansyah9696@gmail.com>
2025-08-16 14:36:10 +0000

    Revert "syscall: Only fake uname on very first call of netbpfload"
    This reverts commit 9997eab37c997a88448c0f6de92f126fbeedf7c0.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1206,16 +1206,15 @@ static int override_version(struct new_utsname __user *name)
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
 
-static uint64_t netbpfload_pid = 0;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "netbpfload", 10) &&
-	    current->pid != netbpfload_pid) {
-		netbpfload_pid = current->pid;
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "5.4.0");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
