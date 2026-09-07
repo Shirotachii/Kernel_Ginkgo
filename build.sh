9ade3d23c73a872125124735ce7dc391ec827e45
Shiro <ryukenzych@gmail.com>
2026-09-07 19:52:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ setup() {
         rm -rf KernelSU drivers/kernelsu
 
         echo -e "\nKSU Support, let's Make it On\n"
-        curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/shockwave/kernel/setup.sh" | bash -s master
+        curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
         curl https://raw.githubusercontent.com/maxsteeel/nomount/refs/heads/dev/kernel/setup.sh | bash -
 
         sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/configs/ginkgo_defconfig
